44b249e0c6f21abc5d7c7aa086a3bd11beae1bd1
Dan Mercer <danielmercer1@gmail.com>
2017-05-17 09:37:31 -0600

    Added menu to ImagePickActivity.

--- library/src/main/java/net/danmercer/unsplashpicker/ImagePickActivity.java
+++ library/src/main/java/net/danmercer/unsplashpicker/ImagePickActivity.java
@@ -3,6 +3,8 @@ package net.danmercer.unsplashpicker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import net.danmercer.unsplashpicker.util.UnsplashApiUtils;
@@ -25,5 +27,21 @@ public class ImagePickActivity extends AppCompatActivity {
 			Toast.makeText(this, "Error: No Unsplash API key :(", Toast.LENGTH_SHORT).show();
 			finish();
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.uip_picker, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		final int id = item.getItemId();
+		if (id == R.id.uip_action_about) {
+			Toast.makeText(this, "TODO: show About info", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
